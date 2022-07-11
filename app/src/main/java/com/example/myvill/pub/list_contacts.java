1f30ddc732e9cd2ef33c060abfb1c225c964fca4
Aneesh <aneeshad2@gmail.com>
2022-07-11 22:40:05 +0530

    Commiting the Project

--- app/src/main/java/com/example/myvill/pub/list_contacts.java
+++ app/src/main/java/com/example/myvill/pub/list_contacts.java
@@ -2,13 +2,15 @@ package com.example.myvill.pub;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.RenderNode;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -19,18 +21,20 @@ import com.example.myvill.R;
 import com.example.myvill.admin.login_admin;
 import com.example.myvill.db.DBHelper;
 import com.example.myvill.db.MyAdapter;
-import com.example.myvill.db.PreCreateDB;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class list_contacts extends AppCompatActivity {
+public class list_contacts extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     RecyclerView recyclerView;
-    ArrayList<String> name, address, phone;
+    ArrayList<String> name;
+    ArrayList<String> address;
+    ArrayList<String> phone;
     DBHelper DB;
     MyAdapter adapter;
-    ImageView  add;
-
+    ImageView menu, add, call;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     LinearLayout contentView;
 
     static final float END_SCALE = 0.7f;
@@ -42,17 +46,20 @@ public class list_contacts extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerview);
         DB = new DBHelper(this);
         name = new ArrayList<>();
-        PreCreateDB.copyDB(this);
         address = new ArrayList<>();
         phone = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerview);
         contentView = findViewById(R.id.content);
         add = findViewById(R.id.add);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menu = findViewById(R.id.menu_icon);
         adapter = new MyAdapter(this, name, address, phone);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
+        call = findViewById(R.id.call);
         displaydata();
+        navigationDrawer();     //Navigation Drawer
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,13 +68,55 @@ public class list_contacts extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    private void navigationDrawer() {
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else
+                    drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        animateNavigationDrawer();
+    }
+
+
+    private void animateNavigationDrawer() {
+
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        //drawerLayout.setScrimColor(Color.TRANSPARENT);
+
+        drawerLayout.setScrimColor(getResources().getColor(R.color.colorAccent));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
 
     }
 
 
-    private void displaydata() {
-
+    public void displaydata() {
         Cursor cursor = DB.getdata();
         if (cursor.getCount() == 0) {
             Toast.makeText(list_contacts.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
@@ -77,14 +126,40 @@ public class list_contacts extends AppCompatActivity {
                 name.add(cursor.getString(0));
                 address.add(cursor.getString(1));
                 phone.add(cursor.getString(2));
+
             }
+
         }
 
     }
 
-    public void call(View view) {
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Intent intent1 = new Intent(getApplicationContext(), list_contacts.class);
+                startActivity(intent1);
+                break;
+            case R.id.nav_services:
+                Intent intent2 = new Intent(getApplicationContext(), revenue.class);
+                startActivity(intent2);
+                break;
+            case R.id.nav_news:
+                Intent intent3 = new Intent(getApplicationContext(), news.class);
+                startActivity(intent3);
+                break;
+            case R.id.nav_land:
+                Intent intent4 = new Intent(getApplicationContext(), land.class);
+                startActivity(intent4);
+                break;
+        }
+        return true;
     }
 
-
+//    public void add(String view) {
+//        Intent intent = new Intent(list_contacts.this, login_admin.class);
+//        startActivity(intent);
+//    }
 }
